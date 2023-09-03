d698ae78f4dcc9d6aceb0bf1625412177ac9b40a
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-03 20:08:17 +0500

    Added SearchInsert Problrm

---
+++ Leetcode Practice/SearchInsert.cs
@@ -0,0 +1,37 @@
+﻿public class Solution
+{
+    public static int SearchInsert(int[] nums, int target)
+    {
+        int lo = 0;
+        int hi = nums.Length - 1;
+        int mid = lo + (hi - lo) / 2;
+
+        while (lo <= hi)
+        {
+            if (nums[mid] > target)
+            {
+                hi = mid - 1;
+                mid = lo + (hi - lo) / 2;
+            }
+            else if (nums[mid] < target)
+            {
+                lo = mid + 1;
+                mid = lo + (hi - lo) / 2;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return lo;
+
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] nums = { -1, 0, 3, 5, 9, 12 };
+        int target = 13;
+        int res = Solution.SearchInsert(nums, target);
+        Console.WriteLine(res);
+    }
+}
