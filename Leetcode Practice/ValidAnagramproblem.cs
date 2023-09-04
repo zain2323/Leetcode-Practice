f20ce9635984fa51b967164a1a6100101764716d
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-04 23:53:24 +0500

    Added isAnagramHashmap approach in ValidAnagramProblem

--- Leetcode Practice/ValidAnagramproblem.cs
+++ Leetcode Practice/ValidAnagramproblem.cs
@@ -8,4 +8,21 @@ public class ValidAnagramProblem
         t = String.Concat(t.OrderBy(c => c));
         return s == t;
     }
+
+    public bool isAnagramHashmap(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        Dictionary<char, int> countS = new Dictionary<char, int>();
+        Dictionary<char, int> countT = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            countS[s[i]] = 1 + countS.GetValueOrDefault(s[i], 0);
+            countT[t[i]] = 1 + countT.GetValueOrDefault(t[i], 0);
+        }
+        foreach (char key in countS.Keys)
+        {
+            if (countS[key] != countT.GetValueOrDefault(key, 0)) return false;
+        }
+        return true;
+    }
 }
