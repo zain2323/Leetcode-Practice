4d7c16ebf5708e0c9e6f36ee42726999039b7843
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-06 00:29:48 +0500

    Added TwoSum Problem

---
+++ Leetcode Practice/TwoSumProblem.cs
@@ -0,0 +1,36 @@
+﻿public class TwoSumProblem
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[] TwoSumHashMap(int[] nums, int target)
+    {
+        Dictionary<int, int> prevMap = new Dictionary<int, int>();
+        int i = 0;
+        foreach (var item in nums)
+        {
+            int diff = target - item;
+            if (prevMap.ContainsKey(diff))
+            {
+                return new int[] { prevMap[diff], i };
+            }
+            prevMap[item] = i;
+            i += 1;
+        }
+        return new int[] { -1, -1 };
+    }
+}
