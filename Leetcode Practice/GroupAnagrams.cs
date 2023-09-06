bede0a7c9e42d71ec8dfbc9e5e6a8a1a2455f88b
Zain Siddiqui <57249519+zain2323@users.noreply.github.com>
2023-09-07 00:01:47 +0500

    Added GroupAnagrams Problem

---
+++ Leetcode Practice/GroupAnagrams.cs
@@ -0,0 +1,45 @@
+﻿
+public class GroupAnagramsProblem
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, IList<string>> sortedDict = new Dictionary<string, IList<string>>();
+        foreach (string str in strs)
+        {
+            string anagram = String.Concat(str.OrderBy(c => c));
+            if (sortedDict.ContainsKey(anagram))
+            {
+                sortedDict[anagram].Add(str);
+            }
+            else
+            {
+                sortedDict[anagram] = new List<string> { str };
+            }
+        }
+        return sortedDict.Values.ToList();
+    }
+
+    public IList<IList<string>> GroupAnagramsHashmap(string[] strs)
+    {
+        Dictionary<string, IList<string>> result = new Dictionary<string, IList<string>>();
+        foreach (string str in strs)
+        {
+            int[] count = new int[26];
+            foreach (char c in str)
+            {
+                int idx = (int)c - (int)'a';
+                count[idx] += 1;
+            }
+            if (result.ContainsKey(String.Join(",", count)))
+            {
+                result[String.Join(",", count)].Add(str);
+            }
+            else
+            {
+                result.Add(String.Join(",", count), new List<string>() { str });
+            }
+        }
+        return result.Values.ToList();
+    }
+}
+
